7217efdc001759f1232b857898588a2817b3fd49
JulioNervar <julio.nervar@achievewithoutborders.org>
2016-04-21 04:20:27 -0700

    perinfo

---
+++ ex2.py
@@ -0,0 +1,29 @@
+#BIODATA
+
+print "******PLEASE FILL UP YOUR INFORMATION*******\n\n"
+print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+name = raw_input("Name     : ") 
+name1 = raw_input("Lastname : ") 
+address = raw_input("Address  : ")
+age= raw_input("Age      : ")
+while age.isalpha():
+    age= raw_input("Age      : ")
+mobile= raw_input("Contact# : ")
+while mobile.isalpha():
+    mobile= raw_input("Contact# : ")
+hobby=  raw_input("Hobbies  : ")
+print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+school=raw_input("School Name : ")
+year = raw_input("Year        : ")
+course= raw_input("Course      : ")
+print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+print "             PERSONAL INFORMATION            "
+print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+print "     Good day! my name is " + name + " " + name1  + ". " + "I live at " + address + "."
+print "     I am " + age + " years old. My contact number is " + mobile + " . " + "My hobby is "+ hobby + "."
+print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+print "                    SCHOOL                   "
+print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+print "     Currently a " + year + " student taking up " + course + " in " + school
